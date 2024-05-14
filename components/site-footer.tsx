b01d9b94f0b9b90ec0ca35d85b0c43bf25b2bcd6
namsgit <namanbhulawat@gmail.com>
2024-05-14 18:10:34 +0530

    v0

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,9 +1,26 @@
-const SiteFooter = () => {
-    return (
-        <footer>
-            Naman Footer
-        </footer>
-    ) 
-}
+import { siteConfig } from "@/config/site";
+import { Mail } from "lucide-react";
+import { Icons } from "./icons";
 
-export default SiteFooter
+export function SiteFooter() {
+  return (
+    <footer>
+      <div className="mb-6 mt-14 flex flex-col items-center">
+        <div className="mb-3 flex space-x-4">
+          <a target="_blank" rel="noreferrer" href={siteConfig.links.mail}>
+            <span className="sr-only">Mail</span>
+            <Mail className="h-6 w-6" />
+          </a>
+          <a target="_blank" rel="noreferrer" href={siteConfig.links.twitter}>
+            <span className="sr-only">Twitter</span>
+            <Icons.twitter className="h-6 w-6" />
+          </a>
+          <a target="_blank" rel="noreferrer" href={siteConfig.links.github}>
+            <span className="sr-only">GitHub</span>
+            <Icons.gitHub className="h-6 w-6" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+    name: "Naman's home",
+    // Update this 
+    url: "https://example.com",
+    description: "Nextjs 14 blog using velite, tailwind and shadcn",
+    author: "thebhulawat",
+    links: {
+      twitter: "https://twitter.com/theBhulawat",
+      github: "https://github.com/thebhulawat",
+      // update this
+      mail: "mailto:namanbhulawat@gmail.com",
+    },
+  };
+  
+  export type SiteConfig = typeof siteConfig;
+
+  
