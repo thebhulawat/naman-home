52a45babe39066b811a7b0cd69ef1d55ac149643
namsgit <namanbhulawat@gmail.com>
2024-05-16 16:43:33 +0530

    added animation to the header

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { siteConfig } from "@/config/site";
-import { Icons } from "./icons";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import "@/styles/animate.css";
 
 
 import { Space_Mono} from 'next/font/google'
@@ -14,10 +14,11 @@ const spaceMono = Space_Mono({
 
 export function MainNav() {
   const pathname = usePathname();
+  const isHomePage = pathname === '/'
   return (
     <nav className="flex items-center space-x-4 lg:space-x-6">
       <Link href="/" className="flex items-center">
-        <span className="text-xl font-bold lowercase text-[#22c55e]" style={{ fontFamily: 'JetBrains Mono' }}>{siteConfig.name}</span>
+        <span className={`text-xl font-bold lowercase text-[#22c55e] ${isHomePage ? 'name-typewriter' : ''}`} style={{ fontFamily: 'JetBrains Mono' }}>{siteConfig.name}</span>
       </Link>
     </nav>
   );
