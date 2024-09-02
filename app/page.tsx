8b99c5df1d9749bad24e822453e864d4c035b445
namsgit <namanbhulawat@gmail.com>
2024-09-02 14:24:19 +0530

    updated links

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center px-8">
         <div className={`grid grid-cols-1 gap-y-1 gap-x-1 w-full mt-[36vh] items-center justify-center justify-items-center align-items`}>
           {list.map((id, index) => (
-            <Link href="/blog" key={id}>
+            <Link href={urls[index]} key={id}>
               <section className={`w-full ${activeIndex === index ? 'active' : ''}`} id={id}>
                 <h2 className={`text-sm lowercase`}>
                   {activeIndex === index && <span className="text-[#22c55e]">&gt;</span>} 
--- app/reflections/page.tsx
+++ app/reflections/page.tsx
@@ -1,6 +1,6 @@
 export default async function Life() {
-    return(
-        <div className="max-w-2xl px-8 py-6 lg:py-10">
+  return (
+    <div className="max-w-2xl px-8 py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 mb-6 md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="mr-2 font-black text-xl lg:text-xl">reflections</h1>
@@ -8,10 +8,11 @@ export default async function Life() {
       </div>
       <div>
         <p className="text-muted-foreground text-base py-4">
-            Life goes on too fast. It&aposs a whirlwind of moments and memories, and we often forget to pause and reflect on the journey we&aposre on. The days may seem long, but the years are short, and before we know it, we&aposre looking back wondering where the time went.
-            </p>
+          Life goes on too fast. It&aposs a whirlwind of moments and memories.
+          The days may seem long, but the years are short, and before we know
+          it, we&aposre looking back wondering where the time went.
+        </p>
       </div>
     </div>
-    )
-
+  );
 }
